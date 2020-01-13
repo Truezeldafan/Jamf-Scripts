7899aba207da07bd772c9fe486b8990df854f866
Truezeldafan <Gt2luvdabears@gmail.com>
2020-01-13 09:56:56 -0600

    Create Compuer Name Change Self Service.sh

---
+++ Compuer Name Change Self Service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ComputerName=`/usr/bin/osascript << EOT
+tell application "System Events"
+    activate 
+    set ComputerName to text returned of (display dialog "Please Input New Computer Name" default answer "" with icon 2)
+end tell
+EOT`
+
+#Set New Computer Name
+echo $ComputerName
+scutil --set HostName $ComputerName
+scutil --set LocalHostName $ComputerName
+scutil --set ComputerName $ComputerName
+
+echo "Rename Successful"
+
+exit 0
